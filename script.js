2c712f973e1b57d842505d318a0dbfd3059e3609
Corentin88 <corentin.lanaud@wanadoo.fr>
2026-08-20 23:29:28 +0200

    feat: render tasks and format dates

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const tasks = [];
 
 function addTask(title, priority) {
@@ -13,3 +12,46 @@ function addTask(title, priority) {
   return newTask;
 }
 console.log(addTask("test", "medium"));
+console.log(addTask("test2", "High"));
+console.log(addTask("test3", "low"));
+
+function renderTasks() {
+  const taskList = document.querySelector("#taskList");
+  taskList.textContent = "";
+  tasks.forEach((task) => {
+    const liElement = document.createElement("li");
+    const titleElement = document.createElement("span");
+    const priorityElement = document.createElement("span");
+    const dateElement = document.createElement("span");
+    titleElement.textContent = task.title;
+    priorityElement.textContent = task.priority;
+    dateElement.textContent = formatDate(task.createdAt);
+    liElement.appendChild(titleElement);
+    liElement.appendChild(priorityElement);
+    liElement.appendChild(dateElement);
+
+    taskList.appendChild(liElement);
+  });
+}
+
+renderTasks();
+
+function formatDate(date) {
+    const dateObject = new Date(date)
+  const day = dateObject.getDate();
+  const month = dateObject.getMonth() + 1;
+  const year = dateObject.getFullYear();
+  const hours = dateObject.getHours();
+  const minutes = dateObject.getMinutes();
+  return (
+    String(day).padStart(2, "0") +
+    "/" +
+    String(month).padStart(2, "0") +
+    "/" +
+    year +
+    " à " +
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minutes).padStart(2, "0")
+  );
+}
